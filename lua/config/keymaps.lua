1778ded3ca996672ea794913f70f4e82b1464d74
Jim Schubert <james.schubert@gmail.com>
2024-03-08 23:15:54 -0500

    feat: trouble+cloak

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- vim.keymap.set("n", "<leader>x", ":bd<cr>", { desc = "Close buffer" })
---
+-- see https://vim.rtorr.com/ for some goodies.
+
+-- taken from ThePrimeagen
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
