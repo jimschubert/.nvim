bd45682d704cc1a674256618350eea3c84f433ff
Jim Schubert <james.schubert@gmail.com>
2024-03-09 10:51:45 -0500

    feat: commands for noice history

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,5 +1,12 @@
 return {
   "folke/noice.nvim",
+  event = "VeryLazy",
+
+  keys = {
+    { "<leader>ne", "<cmd>NoiceErrors<cr>" },
+    { "<leader>nh", "<cmd>NoiceHistory<cr>" },
+  },
+
   opts = {
     presets = {
       lsp_doc_border = true,
