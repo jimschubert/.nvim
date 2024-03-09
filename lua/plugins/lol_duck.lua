4714cd81902872610f9e47c97639111cd7575549
Jim Schubert <james.schubert@gmail.com>
2024-03-09 12:41:30 -0500

    feat: duck cat rawr

---
+++ lua/plugins/lol_duck.lua
@@ -0,0 +1,30 @@
+return {
+  "tamton-aquib/duck.nvim",
+  keys = {
+    { "<leader>hd", "<cmd>HatchDuck<cr>", desc = "Hatch Duck" },
+    { "<leader>hc", "<cmd>HatchCat<cr>", desc = "Hatch Cat" },
+    { "<leader>hr", "<cmd>HatchRawr<cr>", desc = "Hatch Rawr" },
+    { "<leader>hk", "<cmd>HatchKill<cr>", desc = "Kill last one" },
+    { "<leader>ha", "<cmd>HatchKillAll<cr>", desc = "Kill 'em all" },
+  },
+  init = function()
+    local wk = require("which-key")
+    wk.register({ ["<leader>h"] = { name = "+hatch" } })
+
+    vim.api.nvim_create_user_command("HatchDuck", function()
+      require("duck").hatch("🦆", 5)
+    end, {})
+    vim.api.nvim_create_user_command("HatchCat", function()
+      require("duck").hatch("🐈", 5)
+    end, {})
+    vim.api.nvim_create_user_command("HatchRawr", function()
+      require("duck").hatch("🦖", 10)
+    end, {})
+    vim.api.nvim_create_user_command("HatchKill", function()
+      require("duck").cook()
+    end, {})
+    vim.api.nvim_create_user_command("HatchKillAll", function()
+      require("duck").cook_all()
+    end, {})
+  end,
+}
