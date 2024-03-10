aa9c0b712ae9024556a85372955445a4e0ed926d
Jim Schubert <james.schubert@gmail.com>
2024-03-10 13:05:25 -0400

    feat: config twilight in zenmode

---
+++ lua/plugins/twilight.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/twilight.nvim",
+  opts = {
+    dimming = {
+      alpha = 0.35, -- amount of dimming
+      color = { "Normal", "#7e9db7" },
+    },
+    context = 15,
+    exclude = { "yaml" },
+  },
+}
