a19695901daf0e953d43f9c1c9fdab7e90b6e1e0
Jim Schubert <james.schubert@gmail.com>
2024-03-10 12:35:30 -0400

    feat: keymaps better move/escape/center

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,12 +3,22 @@
 -- Add any additional keymaps here
 -- vim.keymap.set("n", "<leader>x", ":bd<cr>", { desc = "Close buffer" })
 -- see https://vim.rtorr.com/ for some goodies.
+local opts = { noremap = true, silent = true }
 
 -- Toggle ZenMode, which will also pop up a fortune. See plugins/zenmode.lua
-vim.keymap.set("n", "<leader>z", ":ZenMode<CR>")
+vim.keymap.set("n", "<leader>z", ":ZenMode<CR>", opts)
+
+-- keymaps for moving in select mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv-gv", opts)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv-gv", opts)
+
+-- keymap for escaping insert and visual mode via CTRL+;
+vim.keymap.set({ "i", "v" }, "<C-;>", "<Esc>", opts)
+
+-- keymap for centering search results
+vim.keymap.set("n", "n", "nzzzv", opts)
+vim.keymap.set("n", "N", "Nzzzv", opts)
 
 -- taken from ThePrimeagen
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], opts)
+vim.keymap.set("n", "<leader>Y", [["+Y]], opts)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- set winbar to show the current file path
 vim.opt.winbar = "%=%m %f"
